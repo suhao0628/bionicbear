4abb85c719a85a055a37f5414acc03ba92d736ab
suhao <suhaoxb@gmail.com>
2025-05-23 04:10:17 +0700

    feat: fixed eslint error :(

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@
 import { useEffect, useState } from 'react'
 
 export default function Home() {
-  const texts = ["世间无我 方见真我", "Flee as a bird to your mountain."]
   const [displayed, setDisplayed] = useState('')
   const [textIndex, setTextIndex] = useState(0)
   const [charIndex, setCharIndex] = useState(0)
 
   useEffect(() => {
+    const texts = ["世间无我 方见真我", "Flee as a bird to your mountain."]
     const currentText = texts[textIndex]
 
     if (charIndex <= currentText.length) {
@@ -25,7 +25,7 @@ export default function Home() {
       }, 2000)
       return () => clearTimeout(pause)
     }
-  }, [charIndex, textIndex, texts])
+  }, [charIndex, textIndex])
 
   return (
     <div className="text-center py-20">
